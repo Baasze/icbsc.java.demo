ba1127f15ae652ed72d5ace9d88210bf79c8c41b
kay20475 <kay20475@hotmail.com>
2020-09-17 14:03:28 +0800

    add sm2 crypto

--- README.md
+++ README.md
@@ -64,6 +64,41 @@ dependencies {
   }
 ```
 
+### crypto
+
+1、 `doEncrypt` and `doDecrypt`:
+
+```java
+import icbsc.java.eckey.ECKey;
+import icbsc.java.crypto.*;
+
+ECKey key = new ECKey();
+System.out.println(key.GetPrivate());
+System.out.println(key.GetPublic());
+
+String msg = "hello world!!!!!!!!!!!!!!!!!!";
+String encrypted = sm2.doEncrypt(msg.getBytes(), key.GetPublic());
+System.out.println(encrypted);
+
+byte[] msgBytes = sm2.doDecrypt(encrypted, key.GetPrivate());
+System.out.println(new String(msgBytes));
+```
+
+2、`doSignature` and `doVerifySignature`
+
+```java
+import icbsc.java.eckey.ECKey;
+import icbsc.java.crypto.*;
+
+ECKey key = new ECKey();
+System.out.println(key.GetPrivate());
+System.out.println(key.GetPublic());
+String msg = "hello world!!!!!!!!!!!!!!!!!!";
+String sigHex = sm2.doSignature(msg.getBytes(), key.GetPrivate());
+System.out.println(sigHex);
+System.out.println("verify: " + sm2.doVerifySignature(msg.getBytes(), sigHex, key.GetPublic()));
+```
+
 ### transaction
 
 please see 'test/java/icbsc/java/demo/transactions'
---
+++ src/test/java/icbsc/java/demo/crypto/sm2Test.java
@@ -0,0 +1,33 @@
+package icbsc.java.crypto;
+import org.junit.Test;
+
+import icbsc.java.eckey.ECKey;
+
+public class sm2Test {
+  @Test
+  public void cryptoTest() throws Exception {
+    for (int i = 0; i < 100; i++) {
+      ECKey key = new ECKey();
+      System.out.println(key.GetPrivate());
+      System.out.println(key.GetPublic());
+
+      String msg = "hello world!!!!!!!!!!!!!!!!!!";
+      String encrypted = sm2.doEncrypt(msg.getBytes(), key.GetPublic());
+      System.out.println(encrypted);
+
+      byte[] msgBytes = sm2.doDecrypt(encrypted, key.GetPrivate());
+      System.out.println(new String(msgBytes));
+      assert(msg.equals(new String(msgBytes)));
+    }
+  }
+  @Test
+  public void signTest() throws Exception {
+    ECKey key = new ECKey();
+    System.out.println(key.GetPrivate());
+    System.out.println(key.GetPublic());
+    String msg = "hello world!!!!!!!!!!!!!!!!!!";
+    String sigHex = sm2.doSignature(msg.getBytes(), key.GetPrivate());
+    System.out.println(sigHex);
+    assert (sm2.doVerifySignature(msg.getBytes(), sigHex, key.GetPublic()));
+  }
+}
